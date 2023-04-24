bf47c144cbb1ae52e338fcf10050d744f69eb6fd
BinaMPS <jossimoloi@gmail.com>
2023-04-24 11:54:21 -0700

    print

--- _printint.c
+++ _printint.c
@@ -7,3 +7,13 @@ _printint - prints an integer
 Return: number of digits printed
 */
 int _printint(va_list list)
+{
+	int num = va_arg(list, int);
+	int digit_count = 0;
+	int is_negative = 0;
+
+ 	if (num < 0) {
+         	is_negative = 1;
+         	num *= -1;
+         	digit_count++;
+ 	}
