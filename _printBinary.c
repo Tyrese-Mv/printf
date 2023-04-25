a380cf80b28f1fc2a05ae8f3981f974438188676
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-25 13:43:00 -0700

    messed up git pull

--- _printBinary.c
+++ _printBinary.c
@@ -12,7 +12,7 @@ int _printBinary(va_list list)
 {
 	unsigned int n = va_arg(list, unsigned int);
 	int count = 0;
-	int i, result;
+	int i;
 
 	for (i = 31; i >= 0; i--)
 	{
@@ -20,15 +20,15 @@ int _printBinary(va_list list)
 
 		if ((n & mask) == mask)
 		{
-			result += write(1, "1", 1);
+			write(1, "1", 1);
 			count++;
 
 		}
 		else if (count > 0 || i == 0)
 		{
-			result += write(1, "0", 1);
+			write(1, "0", 1);
 			count++;
 		}
 	}
-	return (result);
+	return (count);
 }
