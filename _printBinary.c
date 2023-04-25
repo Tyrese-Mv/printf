fb7ebb69588155dd8e5d40b1589387c22eeb8817
BinaMPS <jossimoloi@gmail.com>
2023-04-25 11:44:53 -0700

    M

--- _printBinary.c
+++ _printBinary.c
@@ -11,24 +11,25 @@
 int _printBinary(va_list list)
 {
 	unsigned int n = va_arg(list, unsigned int);
+	unsigned int mask = 1u << 31; 2 ^ 31
 	int count = 0;
-	int i, result;
 
-	for (i = 31; i >= 0; i--)
+	while (mask != 0)
 	{
-		unsigned int mask = 1 << 1;
-
-		if ((n & mask) == mask)
-		{
-			result += write(1, "1", 1);
-			count++;
-
-		}
-		else if (count > 0 || i == 0)
-		{
-			result += write(1, "0", 1);
-			count++;
-		}
+		if (n & mask)
+	{
+		write(1, "1", 1);
+		count++;
 	}
-	return (result);
+		else if (count != 0)
+	{
+		mask >>= 1;
+	}
+		if (count == 0)
+	{
+		write(1, "0", 1);
+		count++;
+	}
+		return (count);
+	}
 }
