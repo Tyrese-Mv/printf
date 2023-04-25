da6ae880e03cbfe6c56b69c106deb5f99037191a
Tyrese-Mv <musawenkosi106@gmail.com>
2023-04-25 04:11:41 -0700

    reordered the struct on printcheck.c file

--- _printcheck.c
+++ _printcheck.c
@@ -20,7 +20,8 @@ int _printcheck(const char format, va_list list)
 	functionStruct functions[] = {
 		{'c', _printchar}, {'s', _printstring},
 		{'i', _printint}, {'d', _printint},
-		{'\0', NULL}, {'b', _printBinary}
+		{'b', _printBinary},
+		{'\0', NULL}
 	};
 
 	for (i = 0; functions[i].specifier != '\0'; i++)
