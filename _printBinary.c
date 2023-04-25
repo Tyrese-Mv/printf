7bb455e4a688aeabd1cd6457c4aa19f85f1de431
BinaMPS <jossimoloi@gmail.com>
2023-04-25 06:36:32 -0700

    M

--- _printBinary.c
+++ _printBinary.c
@@ -4,24 +4,28 @@
 
 int_printBinary(va_list list)
 {
-	unsigned int num = va_arg(list, unsigned int);
-	int bits = 8 * sizeof(unsigned int);
-	int binary[bits];
-	int toPrint[bits];
-	int i, k = 0;
+	unsigned int n = va_arg(list, unsigned int);
+	int count = 0;
+	int i, results;
 
-	for (i = 0; i < bits; i++)
+	for (i = 31; i >= 0; i--)
 	{
-		binary[i] = num & 1;
-		num >>= 1;
+		unsigned int mask = 1 << 1;
+
+		if ((n & mask) == mask)
+		{
+			result += write(1, "1", 1);
+			count++;
+
+		}
+		else if (count > 0 || i == 0)
+		{
+			result += write(1, "0", 1);
+			count++;
+		}
+
 	}
 
-	for (i = bits - 1; i >= 0; i--)
-	{
-		toPrint[k] = binary[i];
-		k++;
-	}
-
-	return (write(STDOUT_FILENO, toPrint, sizeof(toPrint)));
+	return (result);
 
 }
